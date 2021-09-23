04d26addb16a0e14de716c200c5fd67c68f41c7a
jiit111 <jitumaurya111@gmail.com>
2021-09-23 17:53:59 +0530

    section 7 extending API

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Interface;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public UserRepository(DataContext context , IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<MemberDTO> GetMemberAsync(string username)
+        {
+            return await _context.Users.Where(x => x.UserName == username)
+                   .ProjectTo<MemberDTO>(_mapper.ConfigurationProvider)
+                  .SingleOrDefaultAsync();
+        }
+
+        public  Task<IEnumerable<MemberDTO>> GetMembersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUserAsync()
+        {
+            return await _context.Users.Include(p => p.Photos)
+                          .ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUserName(string username)
+        {
+            return await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
