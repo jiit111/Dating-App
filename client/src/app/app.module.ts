7860af77bd26cf494cfca4c91535555f01ae0370
jiit111 <jitumaurya111@gmail.com>
2021-09-28 09:41:35 +0530

    section 13 reactive form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
@@ -23,6 +21,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +36,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     TestErrorsComponent,
     MemberCardsComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TextInputComponent
   ],
   imports: [
     BrowserModule,
@@ -45,6 +45,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
   ],
   providers: [
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 
@@ -10,19 +12,39 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model : any = {};
+  registerForm : FormGroup;
 
-  constructor(private accountService : AccountService) { }
+  constructor(private accountService : AccountService , private toastr : ToastrService,private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = this.fb.group({
+      username : ['',Validators.required],
+      password : ['',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]],
+      confirmPassword : ['',[Validators.required,this.matchValues('password')]]
+    })
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+       this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn { 
+    return (control : AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : {isMatching: true}
+    }
   }
 
   register(){
-    this.accountService.register(this.model).subscribe(resposne => {
-      console.log(resposne);
-      this.cancel();
-    },error => {
-      console.log(error);
-    })
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe(resposne => {
+    //   console.log(resposne);
+    //   this.cancel();
+    // },error => {
+    //   console.log(error);
+    // })
   }
 
   cancel(){
